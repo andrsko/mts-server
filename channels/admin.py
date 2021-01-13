641ff23d1ca04b97095e0fd9089dba7b522484dd
Andrii Savchenko <andrsvchnko@gmail.com>
2021-01-13 04:48:16 +0200

    Display tags for channels in admin

--- channels/admin.py
+++ channels/admin.py
@@ -7,7 +7,16 @@ class VideoAdmin(admin.ModelAdmin):
     list_filter = ("yt_channel", "tags")
 
 
+class ChannelAdmin(admin.ModelAdmin):
+    list_display = ("id", "get_tags")
+
+    def get_tags(self, obj):
+        return ", ".join([t.name for t in obj.tags.all()])
+
+    get_tags.short_description = "Tags"
+
+
 admin.site.register(Tag)
 admin.site.register(YTChannel)
 admin.site.register(Video, VideoAdmin)
-admin.site.register(Channel)
+admin.site.register(Channel, ChannelAdmin)
